b53a878face2900187e779a886482d3304112560
alexb <alexb68245@gmail.com>
2023-12-09 13:28:10 +0100

    stash

--- examples/src/Main.java
+++ examples/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hey there!");
-        System.out.println("Hi!");
+        System.out.println("Hi there!");
     }
 }
