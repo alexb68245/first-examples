a08d3ce0093ccae2cd06e9bcaad88a32ecfbe894
alexb68245 <140880261+alexb68245@users.noreply.github.com>
2023-12-09 11:51:13 +0100

    Update Main.java

--- examples/src/Main.java
+++ examples/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hey there!");
+        System.out.println("Hey!");
         System.out.println("Hi!");
     }
 }
